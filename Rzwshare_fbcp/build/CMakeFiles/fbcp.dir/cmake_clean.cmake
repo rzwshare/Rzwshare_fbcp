ba02d97d15afce1827a9e67c2149c4a04b9fc716
rzwshare <q.liu@rzwdisplay.com>
2023-06-29 08:47:59 +0800

    rzwshare commit

---
+++ Rzwshare_fbcp/build/CMakeFiles/fbcp.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fbcp.dir/src/display/diff.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/display.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/dma.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/gpu.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/keyboard.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/low_battery.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/mailbox.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/mem_alloc.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/spi.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/statistics.cpp.o"
+  "CMakeFiles/fbcp.dir/src/display/text.cpp.o"
+  "CMakeFiles/fbcp.dir/src/lcd_driver/st7789V2.cpp.o"
+  "CMakeFiles/fbcp.dir/src/lcd_driver/st7796u.cpp.o"
+  "CMakeFiles/fbcp.dir/src/main.cpp.o"
+  "fbcp"
+  "fbcp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fbcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
